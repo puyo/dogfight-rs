8788b1588400b8a4d9abf7e5d0bb1b7c7a4b14cd
Gregory McIntyre <greg@gregorymcintyre.com>
2018-12-25 10:04:52 +1100

    Hmm

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,23 @@
-extern crate amethyst;
+extern crate piston_window;
 
-use amethyst::{
-    core::{frame_limiter::FrameRateLimitStrategy, transform::TransformBundle},
-    input::InputBundle,
-    prelude::*,
-    renderer::{DisplayConfig, DrawFlat2D, Pipeline, RenderBundle, Stage},
-    utils::application_root_dir,
-};
+use piston_window::*;
 
-use std::time::Duration;
-
-mod dogfight;
-mod level;
 mod plane;
-mod systems;
 
-fn main() -> amethyst::Result<()> {
-    amethyst::Logger::from_config(Default::default())
-        .level_for("gfx_device_gl", amethyst::LogLevelFilter::Warn)
-        .level_for("gfx_glyph", amethyst::LogLevelFilter::Error)
-        .start();
-
-    let app_root = application_root_dir();
-
-    let path = format!("{}/resources/display_config.ron", app_root);
-    let config = DisplayConfig::load(&path);
-
-    let pipe = Pipeline::build().with_stage(
-        Stage::with_backbuffer()
-            .clear_target([0.0, 0.0, 0.0, 1.0], 1.0)
-            .with_pass(DrawFlat2D::new()),
-    );
-
-    use crate::dogfight::Dogfight;
-
-    let binding_path = format!("{}/resources/input_config.ron", application_root_dir());
-
-    let input_bundle =
-        InputBundle::<String, String>::new().with_bindings_from_file(binding_path)?;
-
-    let assets_dir = app_root;
-
-    let game_data = GameDataBuilder::default()
-        .with_bundle(RenderBundle::new(pipe, Some(config)).with_sprite_sheet_processor())?
-        .with_bundle(TransformBundle::new())?
-        .with_bundle(input_bundle)?
-        .with(systems::PlaneSystem, "plane_system", &["input_system"]);
-
-    let mut game = Application::build(assets_dir, Dogfight)?
-        .with_frame_limit(
-            FrameRateLimitStrategy::SleepAndYield(Duration::from_millis(2)),
-            144,
-        )
-        .build(game_data)?;
-
-    game.run();
-
-    Ok(())
+fn main() {
+    let mut window: PistonWindow = WindowSettings::new("Hello Piston!", [640, 480])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c, g| {
+            clear([1.0; 4], g);
+            rectangle(
+                [1.0, 0.0, 0.0, 1.0], // red
+                [0.0, 0.0, 100.0, 100.0],
+                c.transform,
+                g,
+            );
+        });
+    }
 }
